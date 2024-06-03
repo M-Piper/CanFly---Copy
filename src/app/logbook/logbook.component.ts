bf88369f1e5b9d6ba4463d2fe056e5f31390985e
M-Piper <margaret.f.piper@gmail.com>
2024-06-03 12:26:17 -0400

    updatedJune2024

--- src/app/logbook/logbook.component.ts
+++ src/app/logbook/logbook.component.ts
@@ -13,6 +13,47 @@ export class LogbookComponent implements OnInit {
   response: any;
   showErrorMessage: boolean = false;
   showDeletedMessage: boolean = false;
+  hiddenColumns: Set<string> = new Set<string>(); // Set to store hidden column keys
+
+
+  columnAliases: { [key: string]: string } = {
+    'date': 'Date',
+    'aircraftTypeID' : 'Plane ID',
+    'engineType' : 'Engine Type',
+    'registration' : 'Registration',
+    'pilotInCommand' : 'Pilot-in-Command',
+    'studentOrCoPilot' : 'Student or Co-Pilot',
+    'activityExercises' : 'Exercises',
+    'singleEngineDayDualTime' : 'Single Day Dual',
+    'singleEngineDayPICTime' : 'Single Day PIC',
+    'singleEngineNightDualTime' : 'Single Night Dual',
+    'singleEngineNightPICTime' : 'Single Night PIC',
+    'multiEngineDayDualTime' : 'Multi Day Dual',
+    'multiEngineDayPICTime' : 'Mult Day PIC',
+    'multiEngineDaySICTime' : 'Multi Day SIC',
+    'multiEngineNightDualTime' : 'Multi Night Dual',
+    'multiEngineNightPICTime' : 'Multi Night PIC',
+    'multiEngineNightSICTime' : 'Multi Night SIC',
+    'instrumentIMC' : 'Instrument',
+    'instrumentHood' : 'Instrument with Hood',
+    'instrumentFTD' : 'Instrument FTD',
+    'instrumentApproachesCount' : 'Instrument Approaches',
+    'crossCountryDayDualTime' : 'X-Country Day Dual',
+    'crossCountryDayPICTime' : 'X-Country Day PIC',
+    'crossCountryNightDualTime' : 'X-Country Night Dual',
+    'crossCountryNightPICTime' : 'X-Country Night PIC',
+    'routeFrom' : 'From',
+    'routeVia' : 'Via',
+    'routeTo' : 'To',
+    'dualInstructionGivenNotes' : 'Dual Instruction Notes',
+    'floatTimeNotes' : 'Float Time Notes',
+    'vFRSimulatorNotes' : 'VFR Sim Notes',
+    'pilotID' :'Pilot ID',
+    'takeOffs':'# of Takeoffs',
+    'landings':'# of Landings'
+  };
+
+
   constructor(private logsService: LogsService) {}
 
   ngOnInit() {
@@ -22,12 +63,14 @@ export class LogbookComponent implements OnInit {
   getLogs() {
     this.logsService.getLogs().subscribe((data: Flight[]) => {
       this.logEntries = data.map(entry => ({ ...entry, isSelected: false }));
-  });
+      this.determineHiddenColumns(); //add to identify hidden columns
+    });
   }
 
   getKeys(): string[] {
     if (this.logEntries.length > 0) {
-      return Object.keys(this.logEntries[0]);
+      // Filter out the 'isSelected' property
+      return Object.keys(this.logEntries[0]).filter(key => key !== 'isSelected');
     }
     return [];
   }
@@ -38,12 +81,13 @@ export class LogbookComponent implements OnInit {
 
   deleteLog() {
     if (this.selectedRowId > -1) {
+       const selectedEntry = this.logEntries[this.selectedRowId];
       this.logsService.deleteLogs(this.selectedRowId).subscribe(
         (response: any) => {
           this.response = response;
           this.getLogs();
           this.selectedRowId = -1; // Clear selectedRowIds after successful deletion
-          this.showDeletedMessage = true;
+          this.showDeleteMessage();
         },
         (error: any) => {
           console.error('Error deleting log:', error);
@@ -53,5 +97,44 @@ export class LogbookComponent implements OnInit {
     } else {
       this.showErrorMessage = true;
     }
+  }
+
+  showDeleteMessage() {
+    this.showDeletedMessage = true;
+    setTimeout(() => {
+      this.showDeletedMessage = false;
+    }, 2000); // Hide the message after 3 seconds
+  }
+  
+
+  determineHiddenColumns() {
+    const keys = this.getKeys();
+    keys.forEach(key => {
+      const columnValues = this.logEntries.map(entry => entry[key]);
+      // Check if all values in the column (except the header) are null or undefined
+      if (columnValues.slice(1).every(value => value === null || value === undefined || value === '')) {
+        this.hiddenColumns.add(key);
+      } else if (key == 'logEntryID'){
+        this.hiddenColumns.add(key);
+      }
+        else if (key == 'CAF'){
+        this.hiddenColumns.add(key);
+      }
+      else {
+        this.hiddenColumns.delete(key);
+      }
+    });
+  }
+
+  isColumnHidden(key: string): boolean {
+    return this.hiddenColumns.has(key);
+  }
+
+  
+  getColumnAlias(key: string): string {
+  const alias = this.columnAliases[key] || key;
+    console.log(`Key: ${key}, Alias: ${alias}`);
+    return this.columnAliases[key] || key; // Return the display alias if available, otherwise return the original key
+  
   }
 }
