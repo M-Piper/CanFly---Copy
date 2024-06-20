162ec12cf7a6ad3afcf02c60f5d75bd6722ad466
M-Piper <margaret.f.piper@gmail.com>
2024-06-20 10:17:46 -0400

    corrected report html and css

--- src/app/logs.service.ts
+++ src/app/logs.service.ts
@@ -15,7 +15,7 @@ export interface pilotName {
   providedIn: 'root'
 })
 export class LogsService {
-//  private readonly APIUrl = "http://localhost:5117/api/CanFly/";
+ //private readonly APIUrl = "http://localhost:5117/api/CanFly/";
   private readonly APIUrl = "https://canfly-backend.azurewebsites.net/api/CanFly/";
   
   constructor(private http: HttpClient) { }
